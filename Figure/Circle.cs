c80a708216a726f488804961c00e141cf9863ca9
Lebedev Timofey <lebedev.timofey007@gmail.com>
2022-09-21 19:21:43 +0300

    Full lesson-7

---
+++ Figure/Circle.cs
@@ -0,0 +1,25 @@
+﻿namespace Figure
+{
+    internal class Circle : Point
+    {
+        private double radius;
+        public Circle(double radius) : base()
+        {
+            this.radius = radius;
+        }
+
+        public Circle(int x, int y, double radius) : base(x, y)
+        {
+            this.radius = radius;
+        }
+        public Circle(int x, int y, ConsoleColor color, double radius) : base(x, y, color)
+        {
+            this.radius = radius;
+        }
+
+        public double Square()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+}
---
+++ Figure/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Figure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.moveX(5);
+            rect.moveY(10);
+            rect.SetColor(ConsoleColor.Red);
+        }
+    }
+}
